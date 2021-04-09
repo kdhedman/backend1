78ac0a276c898625e86c117c2ee04027a445a473
kdhedman <kenth.david.hedman@gmail.com>
2021-04-09 16:19:12 +0200

    removed in pom.xml:     <properties>         <java.version>11</java.version>     </properties>

--- src/main/java/com/example/backend1/controllers/BookController.java
+++ src/main/java/com/example/backend1/controllers/BookController.java
@@ -1,11 +1,30 @@
 package com.example.backend1.controllers;
 
-/**
- * Created by David Hedman <br>
- * Date: 2021-04-09 <br>
- * Time: 14:51 <br>
- * Project: backend1 <br>
- * Copyright: Nackademin <br>
- */
+import com.example.backend1.model.*;
+import com.example.backend1.repos.DAO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 public class BookController {
+
+    private DAO db = new DAO();
+    private List<Book> books = db.getAllBooks();
+
+    @RequestMapping( "/books")
+    public List<Book> getAllBooks(){
+        return books;
+    }
+
+    @RequestMapping("/book/{id}")
+    public Message getBookById(@RequestParam int id){
+        for(Book b : books){
+            if(b.getId() == id) {
+                String message = String.format("Book found: %s - %s", b.getAuthor(), b.getTitle());
+                return new Message(true, message);
+            };
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/backend1/model/Book.java
+++ src/main/java/com/example/backend1/model/Book.java
@@ -1,6 +1,9 @@
 package com.example.backend1.model;
 
-public class Book {
+import lombok.Data;
+
+public @Data class Book {
+
     protected int id;
     protected String title;
     protected String author;
@@ -10,30 +13,6 @@ public class Book {
     public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
-        this.author = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
         this.author = author;
     }
 }
